e031d231c7c6ee6a4c9fdf09e4e548d0b050f564
afeiship <1290657123@qq.com>
2019-04-19 10:25:59 +0800

    add: init

---
+++ README.md
@@ -0,0 +1,10 @@
+# gitlab-ci-notes
+> Gitlab ci notes
+
+## docs
+- https://afeiship.github.io/gitlab-ci-notes/
+
+## fix v1.0.0-alpha.46
+```shell
+npm i -D vuepress@1.0.0-alpha.44 @vuepress/core@1.0.0-alpha.44
+```
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  base: '/gitlab-ci-notes/',
+  title: 'Gitlab ci notes',
+  host: '0.0.0.0',
+  description: 'Gitlab ci notes',
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: 'http://himg.bdimg.com/sys/portrait/item/be10475f686d6c73db00.jpg'
+      }
+    ]
+  ],
+  themeConfig: {
+    sidebar: [
+      {
+        title: 'Get started',
+        collapsable: false,
+        children: ['/javascript/001-get-started.md', '/javascript/002-globby.md']
+      }
+    ]
+  }
+};
